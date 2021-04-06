a17e6d5b15aad48878b4662bc7581adb4c172734
gozdekardas <gozdekardas@gmail.com>
2021-04-06 16:48:37 +0300

    cell

---
+++ MemeMe/Controllers/SentMemesCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SentMemesCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Gozde Kardas on 31.03.2021.
+//  Copyright © 2021 Gozde Kardas. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - SentMemesCollectionViewCell: UICollectionViewCell
+
+class SentMemesCollectionViewCell: UICollectionViewCell {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var memeImageView: UIImageView!
+}
